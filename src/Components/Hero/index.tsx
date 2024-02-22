89c4d2b53cac4409cdbdb95626958b6bc3ba59c9
somilisa <somilisa1010@gmail.com>
2024-02-22 14:01:20 +0100

    feat: added hero section

---
+++ src/Components/Hero/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import background from "../../Assets/Hero-bg.svg";
+import "./style.scss";
+
+function Hero() {
+  return (
+    <section className="hero-container">
+      <div className="hero-content">
+        <div className="hero-bg">
+          <img src={background} alt="background svg" />
+        </div>
+        <header>
+          <div>
+            <h2>Hi,I’m Lisa</h2>
+          </div>
+          <p>Frontend Developer</p>
+        </header>
+        <div className="contact-info">
+          <p>Lisa Okafor</p>
+          <a href="mailto: somilisa1010@gmail.com">somilisa1010@gmail.com</a>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
