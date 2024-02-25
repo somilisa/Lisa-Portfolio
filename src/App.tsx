106b2579c20254ae2bb2e275fdf79dae098f59aa
somilisa <somilisa1010@gmail.com>
2024-02-25 20:06:30 +0100

    feat: added skills section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Hero from "./Components/Hero";
-import Intro from "./Components/Bio";
+import Bio from "./Components/Bio";
+import Skills from "./Components/Skills";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Hero />
-      <Intro />
-      {/* <Skills />
-      <Projects />
+      <Bio />
+      <Skills />
+      {/* <Projects />
       <Footer /> */}
     </div>
   );
---
+++ src/Components/Skills/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./style.scss";
+import whiteBg from "../../Assets/White-bg.svg";
+import HTML from "../../Assets/icon-html.svg";
+import CSS from "../../Assets/icon-css.svg";
+import JS from "../../Assets/icon-javascript.svg";
+import TS from "../../Assets/icon-typescript.svg";
+import react from "../../Assets/icon-react.svg";
+import Next from "../../Assets/icon-nextjs.svg";
+
+function Skills() {
+  return (
+    <section className="black-section">
+      <img src={whiteBg} alt="black-squares" className="top-bg" />
+      <div className="skills">
+        <h3>Skills</h3>
+        <ul className="skills-list">
+          <li>
+            <img src={HTML} alt="" />
+            <p>HTML</p>
+          </li>
+          <li>
+            <img src={CSS} alt="" />
+            <p>(S)CSS</p>
+          </li>
+          <li>
+            <img src={JS} alt="" />
+            <p>Javascript</p>
+          </li>
+          <li>
+            <img src={react} alt="" />
+            <p>React</p>
+          </li>
+          <li>
+            <img src={TS} alt="" />
+            <p>Typescript</p>
+          </li>
+          <li>
+            <img src={Next} alt="" />
+            <p>NextJS</p>
+          </li>
+        </ul>
+      </div>
+      <img src={whiteBg} alt="black-squares" className="bottom-bg" />
+    </section>
+  );
+}
+
+export default Skills;
