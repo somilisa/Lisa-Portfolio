eb938a73005427a5cc0a14b0bdf3e30a099929ec
somilisa <somilisa1010@gmail.com>
2024-03-06 13:16:28 +0100

    style: reduce skills container length on smaller screens

--- src/Components/Skills/index.tsx
+++ src/Components/Skills/index.tsx
@@ -10,7 +10,7 @@ import Next from "../../Assets/icon-nextjs.svg";
 
 function Skills() {
   return (
-    <section className="container black-section">
+    <section className="container black-section" id="skills-section">
       <img src={whiteBg} alt="white-squares" className="top-bg" />
       <div className="skills">
         <h3 className="h3-margin-bottom">Skills</h3>
